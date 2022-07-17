a4d531ea55196c9b9e903744d7c0bac0e42776cb
Rayon Morgan <rayonaray@hotmail.com>
2022-07-17 18:01:48 -0500

    changes to cicd

--- src/test/java/morgan/rayon/travel/TravelApplicationTests.java
+++ src/test/java/morgan/rayon/travel/TravelApplicationTests.java
@@ -3,7 +3,7 @@ package morgan.rayon.travel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest 
 class TravelApplicationTests {
 
 	@Test
